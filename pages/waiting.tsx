e3f9e5217a138339dc588b7137d591b70648795e
Miguel Aguilera <superbrandon2018@gmail.com>
2023-04-04 00:26:40 -0500

    Fix 3D Objects

--- pages/waiting.tsx
+++ pages/waiting.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react"
-import ReactDOM from "react-dom"
 import { motion, MotionConfig } from "framer-motion"
 import { useAnimatedText } from "../hooks/use-animated-text"
 import { Scene } from "../components/Canvas"
@@ -8,24 +7,27 @@ import { transition } from "../components/Transition"
 
 export default function Waiting() {
     
-    const [isOn, setOn] = useState(true)
-    const headerRef = useAnimatedText(isOn ? 8 : 9, transition)
+    // const [isOn, setOn] = useState(true)
+    // const headerRef = useAnimatedText(isOn ? 8 : 9, transition)
   
     return (
-    <MotionConfig transition={transition}>
-      <motion.div
-        className="container"
-        initial={false}
-        animate={{
-          backgroundColor: isOn ? "#c9ffed" : "#ff2558",
-          color: isOn ? "#7fffd4" : "#c70f46"
-        }}
-      >
-        {/* <h1 className="open" children="<h1>" /> */}
-        {/* <h1 className="close" children="</h1>" /> */}
-        <motion.h1 ref={headerRef} />
-        <Scene isOn={isOn} setOn={setOn} />
-      </motion.div>
-    </MotionConfig>
+        <div>
+            Hola
+        </div>
+    // <MotionConfig transition={transition}>
+    //   <motion.div
+    //     className="container"
+    //     initial={false}
+    //     animate={{
+    //       backgroundColor: isOn ? "#c9ffed" : "#ff2558",
+    //       color: isOn ? "#7fffd4" : "#c70f46"
+    //     }}
+    //   >
+    //     {/* <h1 className="open" children="<h1>" /> */}
+    //     {/* <h1 className="close" children="</h1>" /> */}
+    //     <motion.h1 ref={headerRef} />
+    //     <Scene isOn={isOn} setOn={setOn} />
+    //   </motion.div>
+    // </MotionConfig>
   )
 }
